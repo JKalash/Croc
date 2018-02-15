f5d5e1f146ce5f59a7f15dcf237f7859f71b64e7
Joseph <me@jkalash.com>
2018-02-15 16:28:03 +0200

    Delted unnecessary files and unified architecture

--- Sources/JKEmoji.h
+++ Sources/JKEmoji.h
@@ -6,14 +6,12 @@
 //  Copyright © 2018 Joseph Kalash. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+@import Foundation;
 
 //! Project version number for JKEmojiiOS.
-FOUNDATION_EXPORT double JKEmojiiOSVersionNumber;
+FOUNDATION_EXPORT double JKEmojiVersionNumber;
 
-//! Project version string for JKEmojiiOS.
-FOUNDATION_EXPORT const unsigned char JKEmojiiOSVersionString[];
+//! Project version string for JKEmoji
+FOUNDATION_EXPORT const unsigned char JKEmojiVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <JKEmojiiOS/PublicHeader.h>
-
-
--- Tests/LinuxMain.swift
+++
@@ -1,6 +0,0 @@
-import XCTest
-@testable import JKEmojiTests
-
-XCTMain([
-    testCase(JKEmojiTests.allTests),
-])
